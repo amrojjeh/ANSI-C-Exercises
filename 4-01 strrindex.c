9d1bfe2aa5489a301c27d6dcb141b89b01fbc15a
amrojjeh <amrojjeh@gmail.com>
2020-01-15 16:07:55 -0600

    Finished anotha

---
+++ 4-01 strrindex.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINE 1000
+
+int strrindex(char s[], char t[]);
+
+int main()
+{
+	char str[MAXLINE] = "test This is a test, yoooo!";
+	
+	printf("%d", strrindex(str, "test")); // Expected: 15
+	return 0;
+}
+
+int strrindex(char s[], char t[])
+{
+	int x, j, k;
+
+	int tlength = strlen(t);
+
+	if (tlength == 0) return -1;
+
+	for (x = strlen(s) - 1; x >= 0; --x)
+	{
+		for (j = x, k = tlength - 1; k >= 0 && t[k] == s[j]; --j, --k);
+		if (k == -1)
+			return x - tlength + 1;
+	}
+	return -1;
+}
