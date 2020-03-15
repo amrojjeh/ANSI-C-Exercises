dcf59f2400ba9d3ae20881d8ff25a4c1b9b47d93
amrojjeh <amrojjeh@gmail.com>
2020-03-15 01:25:47 -0500

    Updated build so that it takes arguments

--- buildnrun.cmd
+++ buildnrun.cmd
@@ -2,7 +2,23 @@
 SETLOCAL ENABLEEXTENSIONS
 PUSHD "%~dp1"
 call build %1
-IF EXIST "%~n1.exe" "%~n1.exe"
+IF EXIST "%~n1.exe" (
+	SET PROGRAM="%~n1.exe"
+	SHIFT
+	CLS
+	GOTO run
+) ELSE (
+	EXIT /B 1
+)
+
+:run
+if "%1"=="" goto after_loop
+set RESTVAR=%RESTVAR% %1
+shift
+goto run
+:after_loop
+%PROGRAM% %RESTVAR%
+
 CALL clean
 POPD
 ENDLOCAL
